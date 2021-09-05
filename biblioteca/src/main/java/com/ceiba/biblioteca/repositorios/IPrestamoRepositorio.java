ce48324350afae8d4a217a5aed16f2a04cbcbe01
Emanuel Martínez <emartinezpinzon@gmail.com>
2021-09-04 23:35:24 -0500

    Agrega el método findByindentifiaciónUsuario en el repositorio de préstamo

--- biblioteca/src/main/java/com/ceiba/biblioteca/repositorios/IPrestamoRepositorio.java
+++ biblioteca/src/main/java/com/ceiba/biblioteca/repositorios/IPrestamoRepositorio.java
@@ -4,7 +4,9 @@ import com.ceiba.biblioteca.modelos.PrestamoModelo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IPrestamoRepositorio extends CrudRepository<PrestamoModelo, Long> {
-
+    List<PrestamoModelo> findByidentificaciónUsuario(String idUsuario);
 }
